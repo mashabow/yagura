ef005004298ca170cf8ed0dd7224c1cd9548d092
Masaya Nakamura <mashabow@shiromoji.net>
2020-07-25 23:14:21 +0900

    fix: allocate 1GB of memory for the function
    デフォルト（256 MB）だと以下のエラーが出る
    
    > Error: memory limit exceeded. Function invocation was interrupted.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,9 @@ import { queries } from "./queries.example";
 
 export const run = functions
   .region("asia-northeast1")
+  .runWith({
+    memory: "1GB",
+  })
   .https.onRequest(async (req, res) => {
     const scraper = await Scraper.init();
 
