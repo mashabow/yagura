5589854ab0a9040b84aca92401e95d6200767860
Masaya Nakamura <mashabow@shiromoji.net>
2020-07-26 00:06:46 +0900

    feat: run the function every hour
    Firebase のプロジェクト設定から、デフォルトの GCP リソースロケーションを選択しておく必要がある。
    https://console.firebase.google.com/u/0/project/yagura-633cd/settings/general

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,22 +3,30 @@ import { Scraper } from "./scraper";
 import { queries } from "./queries.example";
 import { sendProducts } from "./slack";
 
-export const run = functions
-  .region("asia-northeast1")
-  .runWith({
-    memory: "1GB",
-  })
-  .https.onRequest(async (req, res) => {
-    const scraper = await Scraper.init();
+const functionBuilder = functions.region("asia-northeast1").runWith({
+  memory: "1GB",
+});
 
-    for (const query of queries) {
-      const products = await scraper.fetchProducts(query);
-      functions.logger.log("products", products);
-      await sendProducts(products);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
+const runImpl = async () => {
+  const scraper = await Scraper.init();
 
-    await scraper.close();
+  for (const query of queries) {
+    const products = await scraper.fetchProducts(query);
+    functions.logger.log("products", products);
+    await sendProducts(products);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
 
-    res.sendStatus(200);
+  await scraper.close();
+};
+
+export const run = functionBuilder.https.onRequest(async (req, res) => {
+  await runImpl();
+  res.sendStatus(200);
+});
+
+export const scheduledRun = functionBuilder.pubsub
+  .schedule("every 1 hours")
+  .onRun(async (context) => {
+    await runImpl();
   });
